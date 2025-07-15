1bbdc89dd7697706ccfc2c91161e4982989d5a8d
Guillermo Zuñiga <guillermo.zuniga@envato.com>
2025-07-14 20:35:58 -0600

    adding step on script

--- script/buildkite/test-variable.sh
+++ script/buildkite/test-variable.sh
@@ -8,4 +8,12 @@ env:
 steps:
   - label: Setting ENABLE_PUSH_ANNOTATIONS environment variable
     command: echo ENABLE_PUSH_ANNOTATIONS variable has been set to $ENABLE_PUSH_ANNOTATIONS
+
+  - label: checking env var
+    command: |
+      if [ \"$ENABLE_PUSH_ANNOTATIONS\" == \"true\" ]; then
+        echo \"Variable is true\"
+      else
+        echo \"Variable is $ENABLE_PUSH_ANNOTATIONS\"
+      fi
 "
